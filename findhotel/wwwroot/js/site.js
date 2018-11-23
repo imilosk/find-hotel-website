05c1ae1c898425d72a9d915e99613b36787ce70d
lukabreza <luka.breza11@gmail.com>
2018-11-23 01:06:28 +0100

    popup za sobe dodan

--- findhotel/wwwroot/js/site.js
+++ findhotel/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+var image = document.createElement("img");
+var imageParent = document.getElementById("roompicture");
+
+function popup(name, description, price, roomimage) {
+    modal.style.display = "block";
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("description").innerHTML = description;
+    document.getElementById("price").innerHTML = price + "€ per day";
+    if (image.firstChild == null) {
+        image.id = "roompic";
+        image.src = "/images/rooms/" + roomimage;
+        imageParent.appendChild(image);
+    }
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
